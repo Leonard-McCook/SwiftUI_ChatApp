eb2cacf44acc06a084ac783eebb9d3c71122f5be
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-12-22 16:17:52 -0500

    - Add Firebase SDK - Add AuthViewModel with Auth methods

---
+++ ChatApp/ViewModels/AuthViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  AuthViewModel.swift
+//  ChatApp
+//
+//  Created by Leonard McCook-Carr on 12/22/22.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthViewModel {
+    
+    static func isUserLoggedIn() -> Bool {
+        return Auth.auth().currentUser != nil
+    }
+    
+    static func getLoggedInUserId() -> String {
+        return Auth.auth().currentUser?.uid ?? ""
+    }
+    
+    static func logout() {
+        try? Auth.auth().signOut()
+    }
+}
