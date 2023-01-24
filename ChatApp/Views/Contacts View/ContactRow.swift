8895395a3a1058ac4ea55ac92f39548d7f21ffa1
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-01-24 16:55:53 -0500

    Create ContactRow file in the Contacts View folder

---
+++ ChatApp/Views/Contacts View/ContactRow.swift
@@ -0,0 +1,20 @@
+//
+//  ContactRow.swift
+//  ChatApp
+//
+//  Created by Leonard McCook-Carr on 1/24/23.
+//
+
+import SwiftUI
+
+struct ContactRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContactRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactRow()
+    }
+}
