fe52e3c0cb544c3a9bdfb5a1ff55560bea107ded
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-02-02 22:06:51 -0500

    Create a new color set named text-header

--- ChatApp/Views/Chats View/ConversationView.swift
+++ ChatApp/Views/Chats View/ConversationView.swift
@@ -17,7 +17,7 @@ struct ConversationView: View {
             
             // Chat header
             HStack {
-                VStack {
+                VStack (alignment: .leading) {
                     
                     
                     // Back arrow
@@ -30,25 +30,49 @@ struct ConversationView: View {
                             .resizable()
                             .scaledToFit()
                             .frame(width: 24, height: 24)
-                            .foregroundColor(Color("text-input"))
+                            .foregroundColor(Color("text-header"))
                     }
                   
                     // Name
                     Text("Marcus Fenix")
                         .font(Font.chatHeading)
-                        .foregroundColor(Color("text-input"))
+                        .foregroundColor(Color("text-header"))
                 }
+                
+                Spacer()
                 
                 // Profile image
                 ProfilePicView(user: User())
             }
+            .frame(height: 104)
             
             // Chat log
             ScrollView {
                 
             }
+            .background(Color("background"))
                 
             // Chat message bar
+            ZStack {
+                Color("background")
+                    
+                
+                HStack {
+                    // Camera button
+                    Button {
+                        // TODO: Show picker
+                    } label: {
+                        Image(systemName: "camera")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                    }
+                    // Textfield
+                    // Send button
+                    
+                }
+            }
+            .frame(height: 76)
         }
         
     }
@@ -56,6 +80,6 @@ struct ConversationView: View {
 
 struct ConversationView_Previews: PreviewProvider {
     static var previews: some View {
-        ConversationView()
+        ConversationView(isChatShowing: .constant(false))
     }
 }
