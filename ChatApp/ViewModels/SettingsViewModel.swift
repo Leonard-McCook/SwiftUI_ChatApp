874de42ece4bd9454f1c7889cb698fc2d9c32c71
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-03-16 17:25:25 -0400

    Add SettingsViewModel file to the ViewModels folder

---
+++ ChatApp/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewModel.swift
+//  ChatApp
+//
+//  Created by Leonard McCook-Carr on 3/16/23.
+//
+
+import Foundation
+
+class SettingsViewModel: ObservableObject {
+    
+    
+    
+}
