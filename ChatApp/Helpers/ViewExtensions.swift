0536bc7a7fe78fa0d00c71ee9a9c148f940c469b
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-03-27 08:04:38 -0400

    Create ViewExtensions file in the Helpers folder

---
+++ ChatApp/Helpers/ViewExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  ViewExtensions.swift
+//  ChatApp
+//
+//  Created by Leonard McCook-Carr on 3/27/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+            
+            ZStack(alignment: alignment) {
+                placeholder().opacity(shouldShow ? 1 : 0)
+                self
+            }
+        }
+}
+
+
+
